46db9ab08d3a9dde5495dadcfbc4fe6f7acc5a4a
Svetli280305 <syotov2021@elam.co.uk>
2022-11-28 10:40:49 +0000

    made movement

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,27 +4,60 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private float moveInput;
 
-    public float fwdSpeed;
     public Rigidbody sphereRB;
-
-    // Start is called before the first frame update
+    
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+    public LayerMask groundLayer;
+    private float moveInput;
+    private float turnInput;
+    private bool isCarGrounded;
+    
+    private float normalDrag;
+    public float modifiedDrag;
+    
+    public float alignToGroundTime;
+    
     void Start()
     {
-        
+        // Detach Sphere from car
+        sphereRB.transform.parent = null;
+        normalDrag = sphereRB.drag;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        // Get Input
         moveInput = Input.GetAxisRaw("Vertical");
-        moveInput *= fwdSpeed * Time.deltaTime;
+        turnInput = Input.GetAxisRaw("Horizontal");
+        // Calculate Turning Rotation
+        float newRot = turnInput * turnSpeed * Time.deltaTime * moveInput;
+        
+        if (isCarGrounded)
+            transform.Rotate(0, newRot, 0, Space.World);
+        // Set Cars Position to Our Sphere
         transform.position = sphereRB.transform.position;
+        // Raycast to the ground and get normal to align car with it.
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
+        
+        // Rotate Car to align with ground
+        Quaternion toRotateTo = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotateTo, alignToGroundTime * Time.deltaTime);
+        
+        // Calculate Movement Direction
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+        
+        // Calculate Drag
+        sphereRB.drag = isCarGrounded ? normalDrag : modifiedDrag;
     }
-
     private void FixedUpdate()
     {
-        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        if (isCarGrounded)
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration); // Add Movement
+        else
+            sphereRB.AddForce(transform.up * -200f); // Add Gravity
     }
 }
